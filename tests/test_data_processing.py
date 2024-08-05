23fdedf6617d1611b76600e28f265c344d4a4ec5
Marcos Macias <markos_macias@hotmail.com>
2024-08-04 20:31:28 -0600

    Uptated structure

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,15 +1,28 @@
 import pandas as pd
 import pytest
-from src.data_processing import drop_empty_columns, fill_na_mode_mean, create_location_dict
+from src.data_processing import clean_data, load_data
 
 # Datos de prueba
 data = {
-    'A': [None, None, 1, 2],
-    'B': [1, 2, None, 4],
-    'Location': ['Sydney', 'Sydney', 'Melbourne', 'Melbourne']
+    'Location': ['Sydney', 'Sydney', 'Melbourne', 'Melbourne'],
+    'Date': ['2017-06-21', '2018-06-21','2019-06-21', '2010-06-21'],
+    'RainToday' : ['Yes', 'No', 'No','Yes'],
+    'RainTomorrow' : ['Yes', 'No', 'No','Yes'],
+    'WindGustDir' : ['N', 'NE', 'S','SW'],
+    'WindDir9am' : ['N', 'N', 'SE','SW'],
+    'WindDir3pm' : ['NE', 'N', 'SE','SW']
 }
 
+@pytest.fixture
+def csv_file(tmpdir):
+    df = pd.DataFrame(data)
+    file_path = tmpdir.join("test_data.csv")
+    df.to_csv(file_path, index=False)
+    return file_path
 
+@pytest.fixture
+def sample_data():
+    return pd.DataFrame(data)
 
 def test_clean_data(sample_data):
     df = clean_data(sample_data)
@@ -20,6 +33,3 @@ def test_clean_data(sample_data):
     assert df['WindGustDir'].dtype == 'float64'
     assert df['WindDir9am'].dtype == 'float64'
     assert df['WindDir3pm'].dtype == 'float64'
-    assert df['WindGustDir'].iloc[0] == 0
-    assert df['WindDir9am'].iloc[0] == 45
-    assert df['WindDir3pm'].iloc[0] == 90
